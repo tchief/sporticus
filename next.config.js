f851d23535c6164db7d3e44f76ee482fbb435bed
tchief <cyber.tchief@gmail.com>
2021-11-27 19:52:00 +0200

    Update for vercel.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/sporticus/" : "",
-  images: {
-    loader: "imgix",
-    path: "https://noop/",
-  },
   NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
   NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
 };
