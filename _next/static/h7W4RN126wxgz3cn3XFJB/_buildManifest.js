ac246df45170f7a5f3e846a5e5961123cfcc6a50
tchief <tchief@users.noreply.github.com>
2021-11-27 17:25:15 +0000

    deploy: 44d8cc34cc320318a65df68cca30823c70d6d30c

--- _next/static/h7W4RN126wxgz3cn3XFJB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/26af6298f33502b4.css","static/chunks/pages/index-53005958ab6db979.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
